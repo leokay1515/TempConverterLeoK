d4566234d7eee3b5e80351729cffa39205da9542
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-13 12:43:44 -0400

    Initial commit

---
+++ TempConverterLeoK/TempConverterLeoKForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TempConverterLeoK
+{
+    partial class frmTempConverterLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 36);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(274, 20);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Enter the temperature in Celcius:";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(318, 36);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(172, 101);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(111, 54);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert To Fahrenheit";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // frmTempConverterLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 177);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblRequest);
+            this.Name = "frmTempConverterLeoK";
+            this.Text = "Temp Converter by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ TempConverterLeoK/TempConverterLeoKForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TempConverterLeoK
+{
+    public partial class frmTempConverterLeoK : Form
+    {
+        public frmTempConverterLeoK()
+        {
+            InitializeComponent();
+        }
+
+        public void ConvertCtoF(double celsius)
+        {
+            //declare fehrenheit
+            double fahrenheit;
+
+            //calculate the answer
+            fahrenheit = ((double) (9) / (double) (5)) * celsius + 32;
+
+            //Display the answer
+            MessageBox.Show(celsius + " celsius is " + fahrenheit + " in fahrenheit.", "Celsius to Fahrenheit");
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //declare celcius
+            double userCelsius;
+
+            //convert the users input to a double
+            userCelsius = Convert.ToDouble(txtCelsius.Text);
+
+            //call the procedure above
+            this.ConvertCtoF(userCelsius);
+        }
+    }
+}
